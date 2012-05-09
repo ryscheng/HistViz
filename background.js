627e4e25d1b3a626e751bbdf9a2c0a33814b3bb4
ryscheng <ryscheng@dev-vm.(none)>
2012-05-08 20:13:08 -0700

    Pulls current window/tab's URL and title Searches for the URL on delicious.com/url/ Prints the associated tags to console

--- background.js
+++ background.js
@@ -1,13 +1,40 @@
-<script>
 init();
 
 function init() {
   console.log("HistViz init()");
-  chrome.browserAction.onClicked.addListener(startHistVizTab);
+  chrome.browserAction.onClicked.addListener(getCurrentTab);
 }
 
-function startHistVizTab(tab) {
-  console.log(tab);
+function getCurrentTab(tab) {
+  chrome.tabs.query({active: true, windowId: chrome.windows.WINDOW_ID_CURRENT}, getKeywords);
+  startHistViz();
+}
+
+function getKeywords(tabs) {
+  var tab = tabs[0];
+  console.log("New HistViz for URL="+tab.url+", Title="+tab.title);
+  var domains = tab.url.split("/");
+  searchDeliciousUrl(domains[2]);
+}
+
+function searchDeliciousUrl(url) {
+  function parseDomForTags(dom) {
+    var taglinks = $("a[rel='tag']",dom);
+    console.log(taglinks)
+    for (var i=0; i<taglinks.length; i++) {
+      var taglink = taglinks[i];
+      console.log(taglink.text);
+    }
+  }
+  var deliciousUrl = "http://delicious.com/url/"+url;
+  console.log("Retrieving tags from "+deliciousUrl)
+  $.get(deliciousUrl, {}, parseDomForTags);
+}
+
+function startHistViz() {
   chrome.tabs.create({url:chrome.extension.getURL("graph_test.html")});
 }
-</script>
+
+function printTab(tab) {
+  console.log(tab);
+}
