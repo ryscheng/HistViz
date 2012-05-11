c73af09eee270c5a7b99e2fc10e3ccddc21f6ac3
Brandon Holt <holt.bg@gmail.com>
2012-05-11 00:00:57 -0700

    New fake graph data that looks more like browser history...

--- graph_test.js
+++ graph_test.js
@@ -27,8 +27,8 @@ var Log = {
 
 function init(){
     //init data
-    var json = {
-        "id": "347_0",
+    var json_old = {
+        "id": "root",
         "name": "Nine Inch Nails",
         "data": {
             "category": "page"
@@ -48,7 +48,8 @@ function init(){
                     "band": "Jerome Dillon",
                     "relation": "member of band",
                     "$color": "#FFFF00",
-                    "size": "1000"
+                    "$size": "1000",
+                    "category": "page"
                 },
                 "children": []
             }, {
@@ -56,7 +57,8 @@ function init(){
                 "name": "nearLY",
                 "data": {
                     "band": "Jerome Dillon",
-                    "relation": "member of band"
+                    "relation": "member of band",
+                    "category": "page"
                 },
                 "children": []
             }]
@@ -351,6 +353,123 @@ function init(){
         }],
         "data": []
     };
+    var json = {
+        "id": "root",
+        "name": "Yelp - seafood boston",
+        "data": {
+            "category": "page",
+            "url": "http://yelp.com/c/boston/seafood",
+            "visited_date": "Sun, Oct. 23",
+            "visited_time": "10:03am"
+        },
+        "children": [{
+            "id": "120",
+            "name": "restaurants",
+            "data": {
+                "category": "tag",
+            },
+            "children": [{
+                "id": "121",
+                "name": "Yelp - clam chowder",
+                "data": {
+                    "category": "page",
+                    "url": "http://yelp.com/c/clam",
+                    "visited_date": "Sun, Oct. 23",
+                    "visited_time": "10.30am"
+                },
+                "children": []
+            }, {
+                "id": "122",
+                "name": "seafood",
+                "data": {
+                    "category": "tag"
+                },
+                "children": []
+            }, {
+                "id": "123",
+                "name": "american",
+                "data": {
+                    "category": "tag"
+                }
+            }, {
+                "id": "124",
+                "name": "Yelp - Neptune Oyster",
+                "data": {
+                    "category": "page",
+                    "url": "http://www.yelp.com/biz/neptune-oyster-boston",
+                    "visited_date": "Sat, June 1",
+                    "visited_time": "5:33pm"
+                }
+            }]
+        }, {
+            "id": "130",
+            "name": "Clam chowder Boston",
+            "data": {
+                "category": "page",
+                "url": "http://www.yelp.com/search?find_desc=clam+chowder",
+                "visited_date": "Sun, Oct. 23",
+                "visited_time": "10.30am"
+            },
+            "children": [{
+                "id": "131",
+                "name": "Yelp - Neptune Oyster",
+                "data": {
+                    "category": "page",
+                    "url": "http://www.yelp.com/biz/neptune-oyster-boston",
+                    "visited_date": "Sat, June 1",
+                    "visited_time": "5:33pm"
+                }
+            }, {
+                "id": "132",
+                "name": "Atlantic Fish",
+                "data": {
+                    "category": "page",
+                    "url": "http://www.atlanticfishco.com/",
+                    "visited_date": "Sat, June 1",
+                    "visited_time": "5:35pm"
+                }
+            }]
+        }, {
+            "id": "140",
+            "name": "attractions",
+            "data": {
+                "category": "tag"
+            },
+            "children": [{
+                "id": "141",
+                "name": "museums",
+                "data": {
+                    "category": "tag"
+                }
+            }, {
+                "id": "142",
+                "name": "historical landmarks",
+                "data": {
+                    "category": "tag"
+                }
+            }, {
+                "id": "143",
+                "name": "Boston Symphony Orchestra",
+                "data": {
+                    "category": "page",
+                    "url": "http://bso.org/",
+                    "visited_date": "Mon, June 8",
+                    "visited_time": "4:44pm"
+                },
+                "children": []
+            }]
+        }, {
+            "id": "150",
+            "name": "things to do in boston - Google",
+            "data": {
+                "category": "page",
+                "url": "https://www.google.com/?q=things+to+do+in+boston",
+                "visited_date": "Sun, Oct 23",
+                "visited_time": "12:03am"
+            },
+            "children": []
+        }]
+    }
     //end
     var infovis = document.getElementById('infovis');
     var w = infovis.offsetWidth - 50, h = infovis.offsetHeight - 50;
